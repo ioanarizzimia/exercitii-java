dac01091f6a5a82a1de437aa301c56912271c1ee
Ioana <ioanairimia788@gmail.com>
2024-09-28 22:54:15 +0300

    calcul viteza

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int distanta, timp, viteza;
+        distanta = 50;
+        timp = 3;
+        viteza = distanta / timp;
+
+        System.out.println("Viteza: " + viteza);
+    }
+}
